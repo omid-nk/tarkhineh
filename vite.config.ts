43b5f36520d72b5f5dd189296481b58c1cdfafbc
Omid Daliri <omiidnk02@gmail.com>
2025-10-22 14:03:40 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/tarkhineh/", // 👈 برای GitHub Pages خیلی مهمه
 });
